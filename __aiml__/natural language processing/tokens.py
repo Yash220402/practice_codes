6fe1060fdba6a37e2e9b08230cb2a5423db5b70c
Yash220402 <yash220402@gmail.com>
2022-08-28 23:29:08 +0530

    add padding to test sequence

--- __aiml__/natural language processing/tokens.py
+++ __aiml__/natural language processing/tokens.py
@@ -1,7 +1,5 @@
-import tensorflow
-from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
-
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 sentences = [
 	"I love my dog",
@@ -10,14 +8,20 @@ sentences = [
 	"Do you think my dog is amazing?"
 ]
 
-tokenizer = Tokenizer(num_words = 100)
+tokenizer = Tokenizer(num_words = 100, oov_token="<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(sentences)
 
-print(word_index)
-print(sequences)
+padded = pad_sequences(sequences)
+print("\nWord index =", word_index)
+print("\nSequences =", sequences)
+
+# Padding the sequences
+padded = pad_sequences(sequences, padding="post", truncating="post", maxlen=5)
+print("\nPadded Sequences:")
+print(padded)
 
 test_data = [
 	'i really love my dog',
@@ -25,4 +29,8 @@ test_data = [
 ]
 
 test_seq = tokenizer.texts_to_sequences(test_data)
-print(test_seq)
+print("\nTest Sequence =", test_seq)
+
+test_padded = pad_sequences(test_seq, padding="post", truncating="post", maxlen=5)
+print("Padded test sequence:")
+print(test_padded)
